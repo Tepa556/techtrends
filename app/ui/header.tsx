e9b2aaf582c806fedc57527aa757ee605a7c36c2
Tepa556 <132324241+Tepa556@users.noreply.github.com>
2025-06-14 17:47:30 +0600

    обновил авторизацию3

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,3 +1,12 @@
+"use client"
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useThemeStore } from '../lib/ThemeStore';
+import { useSession, signOut } from 'next-auth/react';
+import AuthModal from './authModal';
+import RegModal from './regModal';
+
             <AuthModal
                 isOpen={isAuthModalOpen}
                 onCloseAction={() => setIsAuthModalOpen(false)}
